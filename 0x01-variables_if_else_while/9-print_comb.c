e759e984e7671b2012ff40c52c104148ee1d1758
Brook Shimelis <Brookeshete@gmail.com>
2023-02-17 12:49:37 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,13 @@ int main(void)
 
 	for (i = '0'; i <= '9' ; i++)
 	{
-			putchar(i);
+		putchar(i);
 
-			if (i != '9')
+		if (i != '9')
 		{
-																		putchar(',')
-																		putchar(' ');
-																	}
+			putchar(',')
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
