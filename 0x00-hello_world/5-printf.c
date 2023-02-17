f648ab88ae10f2983edb696f703e883ea3601f89
Brook Shimelis <Brookeshete@gmail.com>
2023-02-17 07:53:55 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/*
- *main - Print a string
- *Description: use printf and print C program that prints exactly "with proper grammar, but the outcome is a piece of art,\n"
- *Return: 0
+/**
+ * main - Print a string
+ * Description: use printf to print "with proper grammar, but the outcome is a piece of art,\n"
+ * Return: 0
  */
 
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 			return (0);
 }
