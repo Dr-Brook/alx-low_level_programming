b9518e0d67f4e98e5c5d155034b80e2f1c91e4ca
Brook Shimelis <Brookeshete@gmail.com>
2023-02-17 13:31:40 -0800

    Numbers

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,9 @@
 
 /**
  * main - Print a string
- * Description: use printf to print "with proper grammar, but the outcome is a piece of art,\n"
+ * Description: use printf to print "with proper grammar, but..."
  * Return: 0
  */
-
 int main(void)
 {
 		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
