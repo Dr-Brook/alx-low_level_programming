a220dbf3734e5a0ada97bd3cf1327ab82eba8708
Brook Shimelis <Brookeshete@gmail.com>
2023-02-17 07:23:36 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#nclude <stdio.h>
 
 /*
  *main - Print a string
  *Description: use printf
- *Return: 0
+ *Return: 1
  */
 
 int main(void)
 {
 		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+			return (1);
 }
