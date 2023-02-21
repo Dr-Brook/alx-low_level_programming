0d82bbb94d0ffbf516985b30fd9c8aad6f2668c1
Brook Shimelis <Brookeshete@gmail.com>
2023-02-21 04:27:09 -0800

    I sometimes suffer from insomnia

---
+++ 1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * description: prints the alphabet, in lowercase,
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{		
+	int x = 'a';
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x)
+	_putchar('\n'_);
+}
