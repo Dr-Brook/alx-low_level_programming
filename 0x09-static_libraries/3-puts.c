8e7452e6d625a864f37c5eee6b582b9049140632
Brook Shimelis <Brookeshete@gmail.com>
2023-03-10 11:59:38 -0800

    task 1 update 5

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,7 +1,16 @@
-#include <stdio.h>
-
-int main(void)
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+
+	int main(void)
+	{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+	}	
 }
