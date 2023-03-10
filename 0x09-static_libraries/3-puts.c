2a391d88d9cb16e3d90bcc23f4e1b6b1d8b8075b
Brook Shimelis <Brookeshete@gmail.com>
2023-03-10 11:39:59 -0800

    task 1 update 4

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,28 +1,7 @@
 #include <stdio.h>
 
-/**
- * _puts - Prints a string to stdout
- * @str: The string to print
- */
-void _puts(char *str)
-{
-    int i = 0;
-
-    while (str[i] != '\0')
-    {
-        putchar(str[i]);
-        i++;
-    }
-    putchar('\n');
-}
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-    _puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return 0;
 }
